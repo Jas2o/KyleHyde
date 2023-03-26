cd4bd624674f03d651867d91de15ac8d4b95e70d
Mila Enpace <2317073+adrot@users.noreply.github.com>
2023-03-26 02:17:49 +0200

    Save as PNG automatically on output folder
    The following code change will automatically save animation frames upon opening, as PNG frames on a folder called ''output''.

--- KyleHyde/WindowImageAnimated.xaml.cs
+++ KyleHyde/WindowImageAnimated.xaml.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace KyleHyde {
     /// <summary>
@@ -12,7 +22,6 @@ namespace KyleHyde {
     /// </summary>
     public partial class WindowImageAnimated : Window {
 
-        private string name;
         private Bitmap[] bmps;
         private BitmapImage[] bitmapImages;
         private int indexLast;
@@ -22,17 +31,27 @@ namespace KyleHyde {
             InitializeComponent();
         }
 
-        public WindowImageAnimated(Bitmap[] bmps, string name) {
+        public WindowImageAnimated(Bitmap[] bmps) {
             InitializeComponent();
 
-            Title += " - " + name;
-            this.name = name;
             this.bmps = bmps;
             bitmapImages = new BitmapImage[bmps.Length];
             indexLast = 0;
 
-            for(int i = 0; i < bmps.Length; i++) {
-                using (MemoryStream stream = new MemoryStream()) {
+            // Create the output folder if it does not exist
+            string outputFolderPath = "output";
+            if (!Directory.Exists(outputFolderPath))
+            {
+                Directory.CreateDirectory(outputFolderPath);
+            }
+
+            for (int i = 0; i < bmps.Length; i++)
+            {
+                string folderName = "output";
+                string fileName = System.IO.Path.Combine(folderName, $"frame_{i}.png");
+                Directory.CreateDirectory(folderName);
+
+                using (FileStream stream = File.Create(fileName)) {
                     bmps[i].Save(stream, ImageFormat.Png);
                     stream.Position = 0;
 
@@ -47,7 +66,7 @@ namespace KyleHyde {
 
             dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += dispatcherTimer_Tick;
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 150);
             dispatcherTimer.Start();
         }
 
@@ -55,16 +74,6 @@ namespace KyleHyde {
             if (indexLast >= bitmapImages.Length)
                 indexLast = 0;
             imageBox.Source = bitmapImages[indexLast++];
-        }
-
-        private void btnSaveFrames_Click(object sender, RoutedEventArgs e) {
-            Directory.CreateDirectory(@".\Export");
-            for (int i = 0; i < bmps.Length; i++) {
-                string outfile = @".\Export\" + name + "_" + i + ".png";
-                bmps[i].Save(outfile);
-                if(i == 0)
-                    Process.Start("explorer.exe", "/select, \"" + outfile + "\"");
-            }
         }
     }
 }
